36c933a8201d20b4fb57e3368c5d9ff6a5662327
Rodrigola3 <71781928+Rodrigola3@users.noreply.github.com>
2020-11-25 23:18:31 -0300

    Add files via upload

--- final.py
+++ final.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 25 20:41:06 2020
-
-@author: User
-"""
-
 import pygame
 import random
 UP= 0
 RIGHT= 1
 DOWN= 2
 LEFT= 3
+
+def posicaocomida():
+    x= random.randint(0,590)
+    y= random.randint(0,590)
+    return (x//10 * 10, y//10 * 10)
+def colisao(com,cob):
+    return (com[0] == cob[0] and com[1] == cob[1])
 
 pygame.init()
 altura= 600
@@ -19,14 +19,14 @@ tela= pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo final')
 comida= pygame.Surface((10,10))
 comida.fill((0,0,255))
-lugar_comida= (random.randint(0,590),random.randint(0,590))
+lugar_comida= posicaocomida()
 cobra= [(200,200), (210,200), (220,200)]
 cobrinha= pygame.Surface((10,10))
 cobrinha.fill((0,255,0))
 direcao= LEFT
-
+Game= True
 clock= pygame.time.Clock()
-while True:
+while Game:
     clock.tick(20)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,6 +40,10 @@ while True:
                 direcao= DOWN
             if event.key == pygame.K_LEFT:
                 direcao= LEFT
+    if colisao(cobra[0],lugar_comida):
+        lugar_comida= posicaocomida()
+        cobra.append((0,0))
+        
     if direcao  == UP:
         cobra[0]= (cobra[0] [0], cobra[0] [1] -10)
     if direcao  == DOWN:
@@ -51,9 +55,21 @@ while True:
      
     for i in range(len(cobra) -1, 0, -1):
         cobra[i]= (cobra[i-1][0], cobra[i-1][1])
-    
+        
+    for per in cobra:
+        if cobra[0][0] == per:
+            pygame.quit()
+            Game= False
+    if cobra[0][0] == tela.get_width():
+        pygame.quit()
+        Game= False
+    if cobra[0][0] == tela.get_height():
+        pygame.quit()
+        Game= False
+        
     tela.fill((0,0,0))
     tela.blit(comida, lugar_comida)
     for posicao in cobra:
         tela.blit(cobrinha,posicao)
-    pygame.display.update()        
+    pygame.display.update()
+print('Fim')        
